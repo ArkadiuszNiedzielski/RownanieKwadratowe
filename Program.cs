42f4b8f6784d4770573c9a120c34d400fdf59e67
Kutafilec <a.niedzielski94@wp.pl>
2022-01-01 20:57:50 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RownanieKwadratowe
+{
+    class RownanieKwadratowe
+    {
+        private
+             double a, b, c, x1, x2, delta;
+
+        public
+             void podajWartosci()
+        {
+            try
+            {
+                Console.WriteLine("Podaj wartość a,b,c. Każdą zatwierdź Enterem");
+                a = double.Parse(Console.ReadLine());
+                if (a == 0)
+                {
+                    Console.WriteLine("Wartość a nie może być zerem. Kończę program");
+                    Environment.Exit(0);
+                }
+                b = double.Parse(Console.ReadLine());
+                c = double.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Nieprawidłowe dane, kończę program");
+            }
+            
+
+               
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            RownanieKwadratowe dzialanie = new RownanieKwadratowe();
+            dzialanie.podajWartosci();
+        }
+    }
+}
