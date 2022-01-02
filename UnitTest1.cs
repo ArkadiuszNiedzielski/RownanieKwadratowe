c861d00dee7ecb374c451cc0e0a3d8bdc50dce90
Kutafilec <63808575+Kutafilec@users.noreply.github.com>
2022-01-02 19:01:06 +0100

    Wrzucenie plików projektu testowego do repo

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RownanieKwadratowe.Testy
+{
+    public class RownanieKwadratoweTesty
+    {
+        [Fact]
+        public void podajWartosciTesty()
+        {
+
+        }
+    }
+}
